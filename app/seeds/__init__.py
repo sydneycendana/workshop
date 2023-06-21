d881207be6657f1ee2bf5ae91fc914e21347c8df
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2023-06-20 18:53:21 -0700

    review images seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,8 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .workshops import seed_workshops, undo_workshops
+from .reviews import seed_reviews, undo_reviews
+from .review_images import seed_review_images, undo_review_images
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,16 +19,22 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
+        undo_review_images()
+        undo_reviews()
         undo_workshops()
+        undo_users()
+    seed_review_images()
     seed_users()
     seed_workshops()
+    seed_reviews()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_review_images()
+    undo_reviews()
     undo_workshops()
     undo_users()
     # Add other undo functions here
