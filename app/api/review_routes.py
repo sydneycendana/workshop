e78abfa4957a34cebb8f2702856aae7e6b0f6090
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2023-06-26 16:04:31 -0700

    review updated

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -6,3 +6,34 @@ import requests
 from app.models import db, Review
 
 review_routes = Blueprint('reviews', __name__)
+
+@review_routes.route('/<int:review_id>', methods=['PUT'])
+@login_required
+def edit_review(review_id):
+    review = Review.query.get(review_id)
+    if review:
+        if review.user_id != current_user.id:
+            return jsonify({'error': 'You are not authorized to edit this review'}), 403
+
+        description = request.form.get('description', review.description)
+        wifi = request.form.get('wifi', review.wifi)
+        if wifi == '':
+            wifi = review.wifi
+
+        pet_friendliness = request.form.get('pet_friendliness', review.pet_friendliness)
+        if pet_friendliness == '':
+            pet_friendliness = review.pet_friendliness
+
+        noise_level = request.form.get('noise_level', review.noise_level)
+        if noise_level == '':
+            noise_level = review.noise_level
+
+        review.description = description
+        review.wifi = wifi
+        review.pet_friendliness = pet_friendliness
+        review.noise_level = noise_level
+
+        db.session.commit()
+        return jsonify({'message': 'Review updated successfully'}), 200
+
+    return jsonify({'error': 'Review not found'}), 404
