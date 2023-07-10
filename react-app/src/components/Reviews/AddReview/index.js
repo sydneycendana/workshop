1764a820dbd72e406aae32de2d178f3c5e2f2cad
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2023-07-09 19:53:14 -0700

    ratings stars working

--- react-app/src/components/Reviews/AddReview/index.js
+++ react-app/src/components/Reviews/AddReview/index.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { createReviewThunk } from "../../../store/reviews";
 import { useModal } from "../../../context/Modal";
 import MySlider from "../../MySlider";
+import { ReactComponent as Star } from "../../../assets/icons/star.svg";
 import "./AddReview.css";
 
 const AddReview = ({ workshopId }) => {
@@ -15,6 +16,11 @@ const AddReview = ({ workshopId }) => {
   const [noiseLevel, setNoiseLevel] = useState(0);
   const [images, setImages] = useState([]);
   const [errors, setErrors] = useState({});
+
+  const [wifiHoveredRating, setWifiHoveredRating] = useState(0);
+  const [petFriendlinessHoveredRating, setPetFriendlinessHoveredRating] =
+    useState(0);
+  const [noiseLevelHoveredRating, setNoiseLevelHoveredRating] = useState(0);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -60,43 +66,122 @@ const AddReview = ({ workshopId }) => {
     setImages(newImages);
   };
 
+  const handleStarHover = (rating, ratingSetter) => {
+    ratingSetter(rating);
+  };
+
+  const handleStarClick = (ratingSetter, rating, ratingHoverSetter) => {
+    ratingSetter(rating);
+    ratingHoverSetter(0); // Reset the hovered rating after clicking
+  };
+
   return (
     <form onSubmit={handleSubmit} className="review-form-container">
-      <h3 className="review-form-title">Add Review</h3>
-      <div>
-        <div>
-          <MySlider
-            value={parseFloat(wifi)}
-            onChange={(event, value) => setWifi(value)}
-            name="Wifi"
-            required
-          />
+      <h3 className="review-form-title">How was your visit?</h3>
+      <div className="ratings-container">
+        <div className="rating-container">
+          <h5>Wifi</h5>
+          <div className="stars-container">
+            {[1, 2, 3, 4, 5].map((rating) => (
+              <svg
+                key={rating}
+                className={`star ${
+                  wifiHoveredRating >= rating || wifi >= rating ? "yellow" : ""
+                }`}
+                onMouseEnter={() =>
+                  handleStarHover(rating, setWifiHoveredRating)
+                }
+                onMouseLeave={() => handleStarHover(0, setWifiHoveredRating)}
+                onClick={() =>
+                  handleStarClick(setWifi, rating, setWifiHoveredRating)
+                }
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 2l3.09 6.32 6.91.96-5 4.86 1.18 6.88L12 17.25l-6.18 3.81 1.18-6.88-5-4.86 6.91-.96L12 2zm0 0" />
+              </svg>
+            ))}
+          </div>
           {errors.wifi && <div>{errors.wifi}</div>}
         </div>
-        <div>
-          <MySlider
-            value={parseFloat(petFriendliness)}
-            onChange={(event, value) => setPetFriendliness(value)}
-            name="Pet Friendliness"
-            required
-          />
+        <div className="rating-container">
+          <h5>Pet Friendliness</h5>
+          <div className="stars-container">
+            {[1, 2, 3, 4, 5].map((rating) => (
+              <svg
+                key={rating}
+                className={`star ${
+                  petFriendlinessHoveredRating >= rating ||
+                  petFriendliness >= rating
+                    ? "yellow"
+                    : ""
+                }`}
+                onMouseEnter={() =>
+                  handleStarHover(rating, setPetFriendlinessHoveredRating)
+                }
+                onMouseLeave={() =>
+                  handleStarHover(0, setPetFriendlinessHoveredRating)
+                }
+                onClick={() =>
+                  handleStarClick(
+                    setPetFriendliness,
+                    rating,
+                    setPetFriendlinessHoveredRating
+                  )
+                }
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 2l3.09 6.32 6.91.96-5 4.86 1.18 6.88L12 17.25l-6.18 3.81 1.18-6.88-5-4.86 6.91-.96L12 2zm0 0" />
+              </svg>
+            ))}
+          </div>
           {errors.petFriendliness && <div>{errors.petFriendliness}</div>}
         </div>
-        <div>
-          <MySlider
-            value={parseFloat(noiseLevel)}
-            onChange={(event, value) => setNoiseLevel(value)}
-            name="Noise Level"
-            required
-          />
+        <div className="rating-container">
+          <h5>Noise Level</h5>
+          <div className="stars-container">
+            {[1, 2, 3, 4, 5].map((rating) => (
+              <svg
+                key={rating}
+                className={`star ${
+                  noiseLevelHoveredRating >= rating || noiseLevel >= rating
+                    ? "yellow"
+                    : ""
+                }`}
+                onMouseEnter={() =>
+                  handleStarHover(rating, setNoiseLevelHoveredRating)
+                }
+                onMouseLeave={() =>
+                  handleStarHover(0, setNoiseLevelHoveredRating)
+                }
+                onClick={() =>
+                  handleStarClick(
+                    setNoiseLevel,
+                    rating,
+                    setNoiseLevelHoveredRating
+                  )
+                }
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 2l3.09 6.32 6.91.96-5 4.86 1.18 6.88L12 17.25l-6.18 3.81 1.18-6.88-5-4.86 6.91-.96L12 2zm0 0" />
+              </svg>
+            ))}
+          </div>
           {errors.noiseLevel && <div>{errors.noiseLevel}</div>}
         </div>
       </div>
       <div className="add-review-description-container">
-        <label>Tell us more:</label>
+        {/* <label>Tell us more:</label> */}
         <textarea
           value={description}
+          placeholder="tell us some more"
           onChange={(event) => setDescription(event.target.value)}
+          style={{ fontFamily: "var(--font-lato)" }}
         />
       </div>
       <div className="add-reviews-images">
