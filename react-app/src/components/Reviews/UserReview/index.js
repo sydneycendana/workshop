fff6fcbec9eded77973aed0353a55ac73d4218b3
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2023-08-11 12:30:13 -0700

    separate user review component

---
+++ react-app/src/components/Reviews/UserReview/index.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import OpenModalButton from "../../OpenModalButton";
+import ImageModal from "../../ImageModal";
+import { ReactComponent as Noise } from "../../../assets/icons/noise.svg";
+import { ReactComponent as Pet } from "../../../assets/icons/pet.svg";
+import { ReactComponent as Wifi } from "../../../assets/icons/wifi.svg";
+import { ReactComponent as More } from "../../../assets/icons/more.svg";
+import { ReactComponent as Upvote } from "../../../assets/icons/upvote.svg";
+import { ReactComponent as Downvote } from "../../../assets/icons/downvote.svg";
+import EditReview from "../EditReview";
+import DeleteReview from "../DeleteReview";
+
+const UserReview = ({ userReview }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  return (
+    <div
+      key={userReview.id}
+      className="user-review-container"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsDropdownVisible(false)}
+    >
+      <div className="review">
+        <div className="review-wrapper">
+          <div className="review-container">
+            <p>
+              By {userReview.user_first_name} on{" "}
+              {new Date(userReview.created_at).toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+                year: "numeric",
+              })}
+            </p>
+            <div className="review-ratings-container">
+              <div className="rating-container">
+                <Noise />
+                <p>{userReview.noise_level}</p>
+              </div>
+              <div className="rating-container">
+                <Pet />
+                <p>{userReview.pet_friendliness}</p>
+              </div>
+              <div className="rating-container">
+                <Wifi />
+                <p>{userReview.wifi}</p>
+              </div>
+            </div>
+            <p>{userReview.description}</p>
+            <div className="review-images-container">
+              {userReview.images &&
+                userReview.images.map((image) => (
+                  <div className="review-image-container">
+                    <OpenModalButton
+                      buttonText={
+                        <img
+                          key={image.id}
+                          src={image.url}
+                          alt="review"
+                          style={{
+                            width: "125px",
+                            height: "125px",
+                            objectFit: "cover",
+                            objectPosition: "center",
+                          }}
+                        />
+                      }
+                      modalComponent={<ImageModal imageUrl={image.url} />}
+                      buttonClassName="review-image-button"
+                    />
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="votes-container">
+            <Upvote />
+            {userReview.total_votes}
+            <Downvote className="thumbs-down-icon" />
+          </div>
+        </div>
+        <div>
+          <div className="review-actions-container">
+            {isHovered && (
+              <button
+                className="dropdown-button"
+                onClick={() => setIsDropdownVisible(!isDropdownVisible)}
+              >
+                <More />
+              </button>
+            )}
+            {isDropdownVisible && (
+              <div className="dropdown-menu">
+                <OpenModalButton
+                  buttonText="Edit"
+                  modalComponent={<EditReview userReview={userReview} />}
+                  buttonClassName="edit-workshop-button"
+                />
+                <OpenModalButton
+                  buttonText="Delete"
+                  modalComponent={<DeleteReview userReview={userReview} />}
+                  buttonClassName="edit-workshop-button"
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserReview;
--- react-app/src/components/Workshops/WorkshopDetails/index.js
+++ react-app/src/components/Workshops/WorkshopDetails/index.js
@@ -5,17 +5,13 @@ import { fetchWorkshopById } from "../../../store/workshops";
 import OpenModalButton from "../../OpenModalButton";
 import EditWorkshop from "../EditWorkshop";
 import DeleteWorkshop from "../DeleteWorkshop";
-import EditReview from "../../Reviews/EditReview";
 import AddReview from "../../Reviews/AddReview";
-import DeleteReview from "../../Reviews/DeleteReview";
 import MyCarousel from "../../Carousel";
-import ImageModal from "../../ImageModal";
 import { ReactComponent as Wifi } from "../../../assets/icons/wifi.svg";
 import { ReactComponent as Noise } from "../../../assets/icons/noise.svg";
 import { ReactComponent as Pet } from "../../../assets/icons/pet.svg";
 import { ReactComponent as More } from "../../../assets/icons/more.svg";
-import { ReactComponent as Upvote } from "../../../assets/icons/upvote.svg";
-import { ReactComponent as Downvote } from "../../../assets/icons/downvote.svg";
+import UserReview from "../../Reviews/UserReview";
 import Review from "../../Reviews/WorkshopReviews";
 import "./WorkshopDetails.css";
 
@@ -27,8 +23,6 @@ const WorkshopDetails = () => {
   const user = useSelector((state) => state.session.user);
 
   const [loading, setLoading] = useState(true);
-  const [isHovered, setIsHovered] = useState(false);
-  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [isAdminDropdownVisible, setIsAdminDropdownVisible] = useState(false);
 
   useEffect(() => {
@@ -133,105 +127,10 @@ const WorkshopDetails = () => {
         </div>
       )}
       <div className="line"></div>
+
       {/* ------------ CURRENT USERS REVIEW ------------ */}
-      {userReview ? (
-        <div
-          key={userReview.id}
-          className="user-review-container"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => {
-            setIsHovered(false);
-            setIsDropdownVisible(false);
-          }}
-        >
-          <div className="review">
-            <div className="review-wrapper">
-              <div className="review-container">
-                <p>
-                  By {userReview.user_first_name} on{" "}
-                  {new Date(userReview.created_at).toLocaleDateString("en-US", {
-                    month: "short",
-                    day: "numeric",
-                    year: "numeric",
-                  })}
-                </p>
-                <div className="review-ratings-container">
-                  <div className="rating-container">
-                    <Noise />
-                    <p>{userReview.noise_level}</p>
-                  </div>
-                  <div className="rating-container">
-                    <Pet />
-                    <p>{userReview.pet_friendliness}</p>
-                  </div>
-                  <div className="rating-container">
-                    <Wifi />
-                    <p>{userReview.wifi}</p>
-                  </div>
-                </div>
-                <p>{userReview.description}</p>
-                <div className="review-images-container">
-                  {userReview.images &&
-                    userReview.images.map((image) => (
-                      <div className="review-image-container">
-                        <OpenModalButton
-                          buttonText={
-                            <img
-                              key={image.id}
-                              src={image.url}
-                              alt="review"
-                              style={{
-                                width: "125px",
-                                height: "125px",
-                                objectFit: "cover",
-                                objectPosition: "center",
-                              }}
-                            />
-                          }
-                          modalComponent={<ImageModal imageUrl={image.url} />}
-                          buttonClassName="review-image-button"
-                        />
-                      </div>
-                    ))}
-                </div>
-              </div>
-              <div className="votes-container">
-                <Upvote />
-                {userReview.total_votes}
-                <Downvote className="thumbs-down-icon" />
-              </div>
-            </div>
-            <div>
-              <div className="review-actions-container">
-                {isHovered && (
-                  <button
-                    className="dropdown-button"
-                    onClick={() => setIsDropdownVisible(!isDropdownVisible)}
-                  >
-                    <More />
-                  </button>
-                )}
-                {isDropdownVisible && (
-                  <div className="dropdown-menu">
-                    <OpenModalButton
-                      buttonText="Edit"
-                      modalComponent={<EditReview userReview={userReview} />}
-                      buttonClassName="edit-workshop-button"
-                    />
-                    <OpenModalButton
-                      buttonText="Delete"
-                      modalComponent={<DeleteReview userReview={userReview} />}
-                      buttonClassName="edit-workshop-button"
-                    />
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div></div>
-      )}
+      {userReview ? <UserReview userReview={userReview} /> : <div></div>}
+
       {/* ------------ OTHER REVIEWS ------------ */}
       {workshop.reviews &&
         workshop.reviews
